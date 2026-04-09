ad132d9f535392e964eed5b5b8282cc022f1eb8f
russel fonta <fontawestbrook99@gmail.com>
2026-04-09 12:15:47 +0100

    feat: quiz game erro fixing

--- quiz.sh
+++ quiz.sh
@@ -69,7 +69,7 @@ echo "------------------------------------------------"
 echo ""
 echo "*******END OF QUIZ *******"
 echo "---------------------"
-      mapfile -t question < <( shuf "$question_file"
+      mapfile -t question < <( shuf "$question_file")
 echo "------------------------------------------------"
 for question_data in "${question[@]}"; do
     IFS='|' read -r question opt_a opt_b opt_c opt_d correct <<< "$question_data"
